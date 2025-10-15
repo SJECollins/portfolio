c5362400648c993a0ef4b035d6dc5fe184e8c8d8
SJECollins <66878688+SJECollins@users.noreply.github.com>
2025-10-15 17:39:38 +0100

    Maybe fixed form validation, we'll see

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,24 +1,35 @@
 window.onload = function () {
-  let messageDisplay = document.getElementById("show-message");
-  let formDisplay = document.getElementById("show-form");
-  document
-    .getElementById("contact-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault();
-      emailjs.sendForm("portfolio_service", "portfolio_template", this).then(
-        function () {
-          console.log("SUCCESS!");
-          window.location.href = "./confirmation";
-        },
-        function (error) {
-          console.log("FAILED...", error);
-          messageDisplay.style.display = "block";
-          messageDisplay.insertAdjacentHTML(
-            "afterbegin",
-            "<p>Sorry, there was a problem sending your message.</p><p>Please try again later.</p>"
-          );
-          formDisplay.style.display = "none";
-        }
-      );
-    });
+  const form = document.getElementById("contact-form");
+  const messageDisplay = document.getElementById("show-message");
+  const formDisplay = document.getElementById("show-form");
+
+  form.addEventListener("submit", function (event) {
+    // Make sure form is valid (issue with some browsers not validating on submit)
+    if (!form.checkValidity()) {
+      form.reportValidity();
+      return;
+    }
+
+    event.preventDefault();
+
+    const sendButton = document.getElementById("send-btn");
+    sendButton.value = "Sending...";
+    sendButton.disabled = true;
+
+    emailjs.sendForm("portfolio_service", "portfolio_template", form).then(
+      function () {
+        console.log("SUCCESS!");
+        window.location.href = "./confirmation";
+      },
+      function (error) {
+        console.log("FAILED...", error);
+        messageDisplay.style.display = "block";
+        messageDisplay.insertAdjacentHTML(
+          "afterbegin",
+          "<p>Sorry, there was a problem sending your message.</p><p>Please try again later.</p>"
+        );
+        formDisplay.style.display = "none";
+      }
+    );
+  });
 };
